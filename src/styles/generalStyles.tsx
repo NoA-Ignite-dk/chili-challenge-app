95f62953d17b37f33f0387a811ffe6e425688c5e
andreavalgeirsdottir <andrea.valgeirsdottir@noaignite.com>
2022-11-23 17:52:07 +0100

    feat: 🎸 Styling scoreboard screen

--- src/styles/generalStyles.tsx
+++ src/styles/generalStyles.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
-
+import Colors from '@src/config/colors';
 // Config
-// import Colors from '../config/colors';
 
 // eslint-disable-next-line import/prefer-default-export
 export const typography = StyleSheet.create({
@@ -17,4 +16,35 @@ export const typography = StyleSheet.create({
 	h3: {
 		fontWeight: '100',
 	},
+
+
+	firstPlacePole:{
+		height: 160,
+		width: 85,
+		backgroundColor: Colors.DARK_GREEN,
+		alignItems: 'center'
+	},
+	secondPlacePole:{
+		height: 120,
+		width: 85,
+		backgroundColor: Colors.DARK_GREEN,
+		alignItems: 'center'
+	},
+	thirdPlacePole: {
+		height: 105,
+		width: 85,
+		backgroundColor: Colors.DARK_GREEN,
+		alignItems: 'center'
+
+	},
 });
+
+export const containerStyles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	padding: {
+		padding: 15,
+	}
+
+})
