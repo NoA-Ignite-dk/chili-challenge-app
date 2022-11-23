23ab5e29483354d0b25dd0ce41b96f20ce61da73
andreavalgeirsdottir <andrea.valgeirsdottir@noaignite.com>
2022-11-23 12:46:52 +0100

    feat: 🎸 Styled header for main tabs

--- src/navigators/tabs/MainBottomTabNavigator.tsx
+++ src/navigators/tabs/MainBottomTabNavigator.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 // Config
 import { ROUTES } from '@src/config/routes';
+import Colors from '@src/config/colors';
 
 // Screens
 import HomeScreen from '@src/screens/HomeScreen';
@@ -13,11 +16,48 @@ import ProfileScreen from '@src/screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
+const styles = StyleSheet.create({
+	iconContainer: {
+		marginRight: 15,
+	},
+});
+
 function MainBottomTabNavigator() {
 	return (
-		<Tab.Navigator>
-			<Tab.Screen name={ROUTES.FEED} component={HomeScreen} />
-			<Tab.Screen name={ROUTES.SCOREBOARD} component={ScoreboardScreen} />
+		<Tab.Navigator
+			screenOptions={{
+				headerStyle: { backgroundColor: '#FAFAFA', height: 124 },
+				headerTitleAlign: 'left',
+				headerTitleStyle: {
+					fontSize: 24,
+				},
+				headerRight: () => (
+					<View style={styles.iconContainer}>
+						<Ionicons name="settings-outline" size={24} color={Colors.BLACK} />
+					</View>
+				)
+			}}>
+			<Tab.Screen name={ROUTES.FEED} component={HomeScreen}
+				options={{
+					headerRight: () => (
+						<View style={styles.iconContainer}>
+							<Ionicons name="notifications-outline" size={24} color={Colors.BLACK} />
+						</View>
+					)
+				}} />
+			<Tab.Screen name={ROUTES.SCOREBOARD} component={ScoreboardScreen}
+			options={{
+				headerStyle: { backgroundColor: Colors.GREEN_PRIMARY, height: 124 },
+				headerTitleStyle: {
+					color: Colors.WHITE,
+					fontSize: 24,
+				},
+				headerRight: () => (
+					<View style={styles.iconContainer}>
+						<Ionicons name="settings-outline" size={24} color={Colors.WHITE} />
+					</View>
+				)
+			}}/>
 			<Tab.Screen name={ROUTES.POST} component={PostScreen} />
 			<Tab.Screen name={ROUTES.INFO} component={InformationScreen} />
 			<Tab.Screen name={ROUTES.PROFILE} component={ProfileScreen} />
