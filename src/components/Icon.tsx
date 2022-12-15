347e8b6ce3eaf5c8ee7bd07c8c2be017da14ced8
andreavalgeirsdottir <andrea.valgeirsdottir@noaignite.com>
2022-12-15 18:34:50 +0100

    feat: 🎸 add posts & claimed points icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -9,6 +9,8 @@ export enum IconType {
 	INFORMATION = 'information',
 	PROFILE = 'profile',
 	LOADING = 'loading',
+	PROFILE_POSTS = 'profile_posts',
+	PROFILE_CLAIMED_POINTS = 'profile_claimed_points'
 }
 
 const IconConfig = {
@@ -131,6 +133,58 @@ const IconConfig = {
 					type="rotate"
 				></animateTransform>
 			</Path>
+		),
+	},
+	[IconType.PROFILE_CLAIMED_POINTS]: {
+		viewBox: '24 24',
+		definition: (
+			<>
+				<Path
+					d="M6.74531 4H17.3132C17.3132 4 16.4325 17.2571 12.0293 17.2571C9.87823 17.2571 8.56783 14.0935 7.79008 10.8571C6.97571 7.46844 6.74531 4 6.74531 4Z"
+					stroke="#36354A"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round" />
+				<Path
+					d="M17.3132 3.99997C17.3132 3.99997 18.2344 3.0173 19 2.99996C20.5 2.966 20.7773 3.99997 20.7773 3.99997C21.0709 4.6095 21.3057 6.19426 19.8967 7.65712C18.4876 9.11997 16.9103 10.4 16.2684 10.8571"
+					stroke="#36354A"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round" />
+				<Path
+					d="M6.74527 3.99998C6.74527 3.99998 5.78547 3.00611 4.99995 2.99998C3.49995 2.98827 3.22264 3.99998 3.22264 3.99998C2.92908 4.6095 2.69424 6.19426 4.1033 7.65712C5.51235 9.11998 7.14823 10.4 7.79004 10.8571"
+					stroke="#36354A"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round" />
+				<Path
+					d="M8.50662 20C8.50662 18.1714 12.0292 17.2571 12.0292 17.2571C12.0292 17.2571 15.5519 18.1714 15.5519 20H8.50662Z"
+					stroke="#36354A"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round" />
+			</>
+		),
+	},
+	[IconType.PROFILE_POSTS]: {
+		viewBox: '24 24',
+		definition: (
+			<>
+				<Path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M21.4189 15.7321C21.4189 19.3101 19.3099 21.4191 15.7319 21.4191H7.94991C4.36291 21.4191 2.24991 19.3101 2.24991 15.7321V7.93212C2.24991 4.35912 3.56391 2.25012 7.14291 2.25012H9.14291C9.86091 2.25112 10.5369 2.58812 10.9669 3.16312L11.8799 4.37712C12.3119 4.95112 12.9879 5.28912 13.7059 5.29012H16.5359C20.1229 5.29012 21.4469 7.11612 21.4469 10.7671L21.4189 15.7321Z"
+					stroke="#36354A"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round" />
+				<Path
+					d="M7.481 14.463H16.216"
+					stroke="#36354A"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round" />
+			</>
 		),
 	},
 };
