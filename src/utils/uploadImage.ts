c8b20eb58d6eca7dea867982ccca95f20073815c
andreavalgeirsdottir <andrea.valgeirsdottir@noaignite.com>
2022-12-27 22:12:56 +0100

    refactor: 💡 improve typing of uploadImage

--- src/utils/uploadImage.ts
+++ src/utils/uploadImage.ts
@@ -1,14 +1,21 @@
 import { POST_IMAGES_BUCKET } from '@src/constants/general';
 import { supabase } from '@src/lib/supabase';
 import { decode } from 'base64-arraybuffer';
+import { ImageInfo } from 'expo-image-picker';
 import { v4 as uuidv4 } from 'uuid';
 
-export const uploadImage = async (pickerResult: any) => {
+export const uploadImage = async (pickerResult: ImageInfo) => {
 	const imageId = uuidv4();
 
-	const { data, error } = await supabase.storage.from(POST_IMAGES_BUCKET).upload(imageId, decode(pickerResult.base64), {
-		contentType: 'image/png',
-	});
+	const { data, error } = await supabase.storage
+		.from(POST_IMAGES_BUCKET)
+		.upload(
+			imageId,
+			decode(pickerResult.base64 as string),
+			{
+				contentType: 'image/png',
+			}
+		);
 
 	if (error) {
 		throw error;
