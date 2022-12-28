42c72bb47e3051435c0173db9f7bcbe0a2d926c7
andreavalgeirsdottir <andrea.valgeirsdottir@noaignite.com>
2022-12-28 12:32:57 +0100

    feat: 🎸 adding username and image to top three placements

--- src/screens/ScoreboardScreen.tsx
+++ src/screens/ScoreboardScreen.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { containerStyles } from '@src/styles/generalStyles'
+import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { containerStyles, typography } from '@src/styles/generalStyles'
 
 // Components
 
 // Config
 import Colors from '@src/config/colors';
 import ScoreboardCard from '@src/components/ScoreboardCard';
+import { UserWithPoints, useUsersWithPointsQuery } from '@src/data/users-with-points';
+import { ProfileImage } from '@src/components/ProfileImage';
 
 
 const styles = StyleSheet.create({
@@ -18,7 +20,7 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		justifyContent: 'space-around',
 		alignItems: 'flex-end',
-		marginTop: 160
+		marginTop: 60
 	},
 	bottomContainer: {
 		width: '100%',
@@ -45,30 +47,106 @@ const styles = StyleSheet.create({
 		alignItems: 'center'
 
 	},
+	placementContainer: {
+		justifyContent: "center",
+		alignItems: "center",
+
+	},
+	placement: {
+		marginBottom: 10,
+		justifyContent: "center",
+		alignItems: "center",
+
+	},
 	scoreboardNumbers: {
 		fontSize: 50,
 		fontWeight: '800',
-		color: Colors.WHITE,
 		marginTop: 10
 	}
 });
 
 export default function ScoreboardScreen() {
+	const { data: usersWithPoints, isLoading, isError } = useUsersWithPointsQuery();
+
+	if (isLoading) {
+		return (
+			<View>
+				<Text>Loading..</Text>
+			</View>
+		)
+	}
+
+	if (isError || !usersWithPoints) {
+		return (
+			<View>
+				<Text>Something went wrong</Text>
+			</View>
+		)
+	}
+
+	const [first, second, third, ...others] = usersWithPoints
+		.map((user) => {
+			return {
+				...user,
+				total: user.point.reduce((total, point) => total + (point.point_to_claim?.amount || 0), 0)
+			}
+		})
+		.sort((a, b) => b.total - a.total)
+		.map((user, index) => {
+			return {
+				...user,
+				placement: index + 1
+			}
+
+		})
+
+	function renderUserWithPoints({ item }: { item: UserWithPoints & { total: number , placement: number } }) {
+		return (
+			<ScoreboardCard
+				name={item.fullName}
+				points={item.total}
+				placement={item.placement}
+				imageSource={item.profilePicture
+					? { uri: item.profilePicture }
+					: require('../../assets/images/chiliplant.jpg')
+				}
+			/>
+		)
+	}
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.topContainer}>
+				<View style={styles.placementContainer}>
+					<View style={styles.placement}>
+						<ProfileImage imageSource={{ uri: second.profilePicture }} size={"large"}></ProfileImage>
+						<Text style={typography.whiteText}>{second.fullName}</Text>
+					</View>
 					<View style={styles.secondPlacePole}>
-						<Text style={styles.scoreboardNumbers}>2</Text>
+						<Text style={[styles.scoreboardNumbers, typography.whiteText]}>2</Text>
+					</View>
+				</View>
+				<View style={styles.placementContainer}>
+					<View style={styles.placement}>
+						<ProfileImage imageSource={{ uri: first.profilePicture }} size={"large"}></ProfileImage>
+						<Text style={typography.whiteText}>{first.fullName}</Text>
 					</View>
 					<View style={styles.firstPlacePole}>
-						<Text style={styles.scoreboardNumbers}>1</Text>
+						<Text style={[styles.scoreboardNumbers, typography.whiteText]}>1</Text>
+					</View>
+				</View>
+				<View style={styles.placementContainer}>
+					<View style={styles.placement}>
+						<ProfileImage imageSource={{ uri: third.profilePicture }} size={"large"}></ProfileImage>
+						<Text style={typography.whiteText}>{third.fullName}</Text>
 					</View>
 					<View style={styles.thirdPlacePole}>
-						<Text style={styles.scoreboardNumbers}>3</Text>
+						<Text style={[styles.scoreboardNumbers, typography.whiteText]}>3</Text>
 					</View>
+				</View>
 			</View>
 			<View style={[styles.bottomContainer, containerStyles.padding]}>
-				<ScoreboardCard name="Laura Toft" points={20} imageSource={require('../../assets/images/chiliplant.jpg')}/>
+				<FlatList data={others} renderItem={renderUserWithPoints} />
 			</View>
 		</View>
 	);
