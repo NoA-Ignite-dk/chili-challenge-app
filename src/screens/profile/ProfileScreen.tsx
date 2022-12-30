cc96d1804e0df8c4bbf3e7a44bf88dad5ae56a50
andreavalgeirsdottir <andrea.valgeirsdottir@noaignite.com>
2022-12-30 15:13:44 +0100

    feat: 🎸 add bottom indicator to active tab in profile

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -61,7 +61,7 @@ export default function ProfileScreen() {
 			case 'allPosts':
 				return <Icon type={IconType.PROFILE_POSTS} stroke={color} />
 			case 'allPlants':
-				return <Icon type={IconType.PROFILE_POSTS} stroke={color} />
+				return <Icon type={IconType.PROFILE_PLANTS} stroke={color} />
 			case 'claimedPointsList':
 				return <Icon type={IconType.PROFILE_CLAIMED_POINTS} stroke={color} />
 			default:
@@ -78,12 +78,13 @@ export default function ProfileScreen() {
 		<TabBar
 			{...props}
 			activeColor={Colors.GREEN_PRIMARY}
-			inactiveColor={Colors.DARK_GREY}
+			inactiveColor={Colors.GREY}
 			style={{ backgroundColor: Colors.WHITE, borderBottomWidth: 0.5, borderBottomColor: Colors.LIGHT_GREY }}
 			tabStyle={{ width: layout.width / 3, marginTop: 10, marginBottom: 10 }}
 			labelStyle={{ display: 'none', height: 0 }}
 			scrollEnabled={false}
-			renderIndicator={() => null}
+			// renderIndicator={() => null}
+			indicatorStyle={{backgroundColor: Colors.GREEN_PRIMARY}}
 			renderIcon={getTabBarIcon}
 		/>
 	);
